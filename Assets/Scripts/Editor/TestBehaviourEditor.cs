bf20ec52cc3d2c3ecdc4de23b1b59583be190a35
Matheus Lessa Rodrigues <matheus.lessa@cejoseleitelopes.com.br>
2017-08-04 09:43:53 -0300

    first

---
+++ Assets/Scripts/Editor/TestBehaviourEditor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TestBehaviour))]
+public class TestBehaviourEditor : Editor
+{
+    int tomate;
+
+    public override void OnInspectorGUI()
+    {
+        TestBehaviour testBehaviour = target as TestBehaviour;
+
+        testBehaviour.propriedade =
+            EditorGUILayout.IntField("Tomate",
+            testBehaviour.propriedade);
+
+        testBehaviour.volume = EditorGUILayout.Slider("Volume",
+            testBehaviour.volume, 0.0f, 100.0f);
+
+        testBehaviour.texto =
+            EditorGUILayout.TextArea(testBehaviour.texto);
+
+        EditorGUILayout.LabelField("BATATA", EditorStyles.boldLabel);
+
+        if( GUILayout.Button("RESETAR TRASNFORM") )
+        {
+            testBehaviour.DoSomething();
+        }
+    }
+}
---
+++ Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBehaviour : MonoBehaviour
+{
+    public Transform lookAtTarget;
+    public float volume=0.5f;
+
+    public int propriedade;
+    public string texto;
+
+    public void DoSomething()
+    {
+        Debug.Log("KOEH");
+        transform.localPosition = Vector3.zero;
+        transform.LookAt(lookAtTarget);
+    }
+}
