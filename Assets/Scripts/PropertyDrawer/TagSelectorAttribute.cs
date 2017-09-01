bd611c78a3918fbbd57b0c76e2364259cc664a4c
Matheus Lessa Rodrigues <matheuslessarodrigues@gmail.com>
2017-09-01 10:31:12 -0300

    solucao fácil pro problema de TagSelector

---
+++ Assets/Scripts/PropertyDrawer/TagSelectorAttribute.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Put this attribute above an int field and it will draw like a tag picker in the inspector.
+/// </summary>
+[System.AttributeUsage( System.AttributeTargets.Field, AllowMultiple = false )]
+public class TagSelectorAttribute : PropertyAttribute
+{
+}
